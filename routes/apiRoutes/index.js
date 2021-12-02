89e0e5ca2d7b90fbd3b6db98f24818448845f311
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-02 11:59:11 -0600

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use(require('./candidateRoutes.js'));
 router.use(require('./partyRoutes.js'));
+router.use(require('./voterRoutes.js'));
 
 module.exports = router;
